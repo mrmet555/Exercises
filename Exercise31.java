49c36aab446581a984175ab74639cba5f26da336
unknown <dmg23461@AN112513-CT103.nvstu.edu>
2015-09-24 17:09:22 -0400

    class exercise 3.1

---
+++ Exercise31.java
@@ -0,0 +1,13 @@
+public class Exercise31 {
+	public static void main(String[] args) {
+			float random;
+					double last = 1287.545622;
+							
+									System.out.printf("String: %2s\n" + "Character: %c\n"
+											+ "Decimal: %4d\n" + "Exponent =%3e\n" +
+													"Floating Point: %8.1f", last, 'a', 100, last, last);
+															
+
+																}
+																}
+
